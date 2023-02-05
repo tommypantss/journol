59cff4d2b121b8ec471ea7abc216353bca899617
Tommypants <47421901+tommypantss@users.noreply.github.com>
2023-02-05 13:17:38 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
-// Import Winston and Sentry
+// Import Winston, Sentry, Chart.js, and Lodash
 const winston = require('winston');
 const Sentry = require('@sentry/node');
+const Chart = require('chart.js');
+const _ = require('lodash');
 
 // Create a Winston logger
 const logger = winston.createLogger({
@@ -35,9 +37,59 @@ const logMessage = (message, level, dsn) => {
   logger.log(level, log);
 }
 
-// Search logs
-const searchLogs = (searchTerm) => {
-  const filteredLogs = logger.transports.console.write.filter(log => log.message.includes(searchTerm));
+// Search logs based on multiple criteria
+const searchLogs = (searchTerm, level, startDate, endDate) => {
+  // Filter logs based on search term
+  let filteredLogs = logger.transports.console.write.filter(log => log.message.includes(searchTerm));
+
+  // Filter logs based on level
+  if (level) {
+    filteredLogs = _.filter(filteredLogs, { 'level': level });
+  }
+
+  // Filter logs based on date range
+  if (startDate && endDate) {
+    filteredLogs = _.filter(filteredLogs, log => {
+      return new Date(log.timestamp) >= startDate && new Date(log.timestamp) <= endDate;
+    });
+  }
+
+  // Generate chart data for visualization
+  const chartData = {
+    labels: filteredLogs.map(log => log.timestamp),
+    datasets: [
+      {
+        label: 'Logs',
+        data: filteredLogs.map(log => log.message),
+        backgroundColor: '#3366CC'
+      }
+    ]
+  };
+
+  // Generate chart
+  new Chart(document.getElementById("log-chart"), {
+    type: 'bar',
+    data: chartData,
+    options: {
+      scales: {
+        xAxes: [{
+          display: true,
+          scaleLabel: {
+            display: true,
+            labelString: 'Timestamp'
+          }
+        }],
+        yAxes: [{
+          display: true,
+          scaleLabel: {
+            display: true,
+            labelString: 'Message'
+          }
+        }]
+      }
+    }
+  });
+
   return filteredLogs;
 }
 
